7017d37ce3a89396c8775afe5fa539433f6b2904
yunyeongyee <105087260+yunyeongyee@users.noreply.github.com>
2022-05-14 11:05:12 +0900

    revise contents

--- 두 정수 사이의 합.js
+++ 두 정수 사이의 합.js
@@ -8,12 +8,12 @@ function solution(a, b) {
 /*___________________________________________________________
 
     function solution(a, b) {
-
+    let answer = 0;
     let min = Math.min(a, b); //가장 작은 수 반환
     let max = Math.max(a, b); //가장 큰 수 반환
     for (let i = min; i <= max; i++) {
         // i를 min이라고 하고 i가 max보다 작거나 같으면 1씩 증가.
-        let answer += i;
+        answer += i;
         // answer = answer + i;
     }
     return answer;
