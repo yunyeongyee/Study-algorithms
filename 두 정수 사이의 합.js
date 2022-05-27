ced5f113c77a218189b16a402d99c60df0883ddd
yunyeongyee <105087260+yunyeongyee@users.noreply.github.com>
2022-05-27 21:46:44 +0900

    Update 두 정수 사이의 합.js

--- 두 정수 사이의 합.js
+++ 두 정수 사이의 합.js
@@ -1,10 +1,23 @@
 function solution(a, b) {
+    var answer = 0;
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+    for (let i=min; i<=max; i++){
+        answer += i;
+    }
+    return answer;
+}
+
+/*___________________________________________________________
+
+function solution(a, b) {
     let answer = 0;
     for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
         answer += i;
     }
     return answer;
 }
+____________________________________________________________*/
 /*___________________________________________________________
 
     function solution(a, b) {
